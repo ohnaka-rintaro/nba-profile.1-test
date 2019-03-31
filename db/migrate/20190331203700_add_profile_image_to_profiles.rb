c6a3b06961b896a488eccbe838a78e87676ad800
tonegawa <open.practice.programming@gmail.com>
2019-03-31 21:28:56 +0000

    comment out

--- db/migrate/20190331203700_add_profile_image_to_profiles.rb
+++ db/migrate/20190331203700_add_profile_image_to_profiles.rb
@@ -1,5 +1,5 @@
 class AddProfileImageToProfiles < ActiveRecord::Migration[5.0]
   def change
-    add_column :profiles, :profile_image, :string
+    #add_column :profiles, :profile_image, :string
   end
 end
