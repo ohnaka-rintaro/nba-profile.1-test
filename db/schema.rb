0b53e49ad6867bcf07eba27e966ef3cc17daa804
tonegawa <open.practice.programming@gmail.com>
2019-05-27 09:33:06 +0000

    user_id remove migration file update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_131222) do
+ActiveRecord::Schema.define(version: 2019_05_27_092653) do
 
   create_table "add_profile_to_comments", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 2019_05_03_131222) do
     t.datetime "updated_at", null: false
     t.integer "topic_id"
     t.string "post_image"
-    t.integer "user_id"
   end
 
   create_table "profiles", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
