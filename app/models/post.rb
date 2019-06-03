d38db6307815bb4108877ea5f6f1e326ea6bb915
tonegawa <open.practice.programming@gmail.com>
2019-06-03 16:04:29 +0000

    add user_id to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :topic
-  #belongs_to :user
+  belongs_to :user
   
   mount_uploader :post_image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_131222) do
+ActiveRecord::Schema.define(version: 2019_06_03_160216) do
 
   create_table "add_profile_to_comments", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
